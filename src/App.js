1860c4f513c96abde050ad6e49ca7bad60c7ddeb
Dylan-HYF <68765633+Dylan-HYF@users.noreply.github.com>
2022-02-23 16:58:20 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import { useState, useEffect, useCallback } from 'react'
+import { BiCalendar } from 'react-icons/bi'
+import Search from './components/Search'
+import AddAppointment from './components/AddAppointment'
+import AppointmentInfo from './components/AppointmentInfo'
+// import './App.css';
 
 function App() {
+  const [appointmentList, setAppointmentList] = useState([])
+  const [query, setQuery] = useState('')
+  const [sortBy, setSortBy] = useState('petName')
+  const [orderBy, setOrderBy] = useState('asc')
+
+  const searched = appointmentList.filter(x => {
+    return (
+      x.petName.toLowerCase().includes(query.toLowerCase()) ||
+      x.ownerName.toLowerCase().includes(query.toLowerCase()) ||
+      x.aptNotes.toLowerCase().includes(query.toLowerCase())
+
+    )
+  }).sort((a, b) => {
+    // ascending or descending
+    let order = orderBy === 'asc' ? 1 : -1
+    return (
+      // a and b are items in the arr, compare them
+      a[sortBy].toLowerCase() < b[sortBy].toLowerCase() ?
+        // when a < b, return -1, otherwise return 1
+        // according to syntaxt of sort()
+        /*
+          function compare(a, b) {
+            if (a should be before b) {
+              return -1;
+            }
+            if (b should be before a) {
+              return 1;
+            }
+            // a must be equal to b
+            return 0;
+          }
+        */
+        -1 * order : 1 * order
+    )
+  })
+  const fetchData = useCallback(() => {
+    fetch('./data.json')
+      .then(res => res.json())
+      .then(setAppointmentList)
+  }, [])
+  useEffect(() => {
+    fetchData()
+  }, [fetchData])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl mb-3">Hello <BiCalendar className="inline-block text-red-400 align-top" /></h1>
+      <AddAppointment lastId={appointmentList.length} addAppointment={appointment => setAppointmentList([...appointmentList, appointment])} />
+      <Search onQueryChange={
+        val => setQuery(val)
+      } query={query} setOrderBy={setOrderBy}
+        orderBy={orderBy}
+        onOrderByChange={mySort => setOrderBy(mySort)}
+        sortBy={sortBy}
+        onSortByChange={mySort => setSortBy(mySort)} />
+      <ul className="divide-y divide-gray-200">
+        {searched.map(appointment => (
+          <AppointmentInfo key={appointment.id} appointment={appointment}
+            onDelete={
+              // id is from child component
+              id => setAppointmentList(appointmentList.filter(x => x.id !== id))
+            } />
+        ))}
+      </ul>
     </div>
   );
 }
